0617ebcfa86c5b3ed6c01529f626ee3cfb2bc18f
4lrick <alrick.rubio@epitech.eu>
2024-01-29 16:54:40 +0100

    [ADD] Fps counter, Speed counter, CollisionPolygon modif

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,18 @@
 extends VehicleBody3D
 
 const MAX_STEER = 0.8
-const ENGINE_POWER = 300
+const ENGINE_POWER = 500
 
 var parking_brake = false
 
 @onready var camera_pivot = $CameraPivot
 @onready var camera_3d = $CameraPivot/Camera3D
 @onready var reverse_camera = $CameraPivot/ReverseCamera
+@onready var speed_counter = $SpeedText/SpeedCounter
 
 var nlook_at
 var initial_transform
+var speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,9 +22,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if Input.is_action_pressed("brake"):
-		linear_velocity = linear_velocity.lerp(Vector3.ZERO, delta * 1.0)
-	elif Input.is_action_pressed("reset"):
+	_handle_parking_brake()
+	if Input.is_action_pressed("reset"):
 		global_position = Vector3.ZERO
 		global_transform = initial_transform
 	elif Input.is_action_pressed("quit"):
@@ -30,17 +31,26 @@ func _physics_process(delta):
 	else:
 		steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 5.0)
 		engine_force = Input.get_axis("down", "up") * ENGINE_POWER
+		speed = int(linear_velocity.length())
+		speed_counter.text = str(speed)
+	_handle_camera(delta)
 
-	# Camera
+func _handle_camera(delta):
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 2.0)
 	nlook_at = nlook_at.lerp(global_position + linear_velocity, delta * 2.0)
 	camera_3d.look_at(nlook_at)
 	reverse_camera.look_at(nlook_at)
 	_check_camera_swtich()
-
+	
 func _check_camera_swtich():
 	if linear_velocity.dot(transform.basis.z) > -5:
 		camera_3d.current = true
 	else:
 		reverse_camera.current = true
+		
+func _handle_parking_brake():
+	if Input.is_action_pressed("brake"):
+		set_brake(50)
+	elif get_brake() == 50:
+		set_brake(0)
