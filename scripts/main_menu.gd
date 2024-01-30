275d99e0f07a3da7934ccdac5c34923a9d584326
4lrick <alrick.rubio@epitech.eu>
2024-01-30 18:50:36 +0100

    [ADD] main menu and settings menu but not finished

---
+++ scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var play_button = $VBoxContainer/PlayButton
+@onready var settings_button = $VBoxContainer/SettingsButton
+@onready var exit = $VBoxContainer/Exit
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if exit.button_pressed:
+		get_tree().quit()
+	elif settings_button.button_pressed:
+		get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
+	elif play_button.button_pressed:
+		get_tree().change_scene_to_file("res://scenes/world.tscn")
